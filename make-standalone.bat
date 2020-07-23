00202c473ed581e2037e921cca260da0cfff0ce8
Ian Burgwin <ian@ianburgwin.net>
2020-07-22 23:05:34 -0700

    make-standalone: fix copying LICENSE.md

--- make-standalone.bat
+++ make-standalone.bat
@@ -7,5 +7,5 @@ copy bin\win32\save3ds_fuse.exe build\custom-install-standalone\bin
 copy bin\README build\custom-install-standalone\bin
 copy custom-install-finalize.3dsx build\custom-install-standalone
 copy extras\windows-quickstart.txt build\custom-install-standalone
-copy LICENSE.md extras\windows-quickstart.txt
+copy LICENSE.md build\custom-install-standalone
 python -m zipfile -c dist\custom-install-standalone.zip build\custom-install-standalone
