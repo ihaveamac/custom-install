2dd6caf12886dfff9489141d53ff7416b6684c84
Ian Burgwin <ian@ianburgwin.net>
2020-07-22 19:34:14 -0700

    make-standalone: add missing --base-name=

--- make-standalone.bat
+++ make-standalone.bat
@@ -1,4 +1,4 @@
-cxfreeze ci-gui.py --target-dir=dist-standalone Win32GUI
+cxfreeze ci-gui.py --target-dir=dist-standalone --base-name=Win32GUI
 mkdir dist-standalone\bin
 copy TaskbarLib.tlb dist-standalone
 copy bin\win32\save3ds_fuse.exe dist-standalone\bin
