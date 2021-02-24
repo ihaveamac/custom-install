1dec5175ead826b29e2e234da923e9d3a4b3e057
Ian Burgwin <ian@ianburgwin.net>
2021-02-24 14:17:17 -0800

    add title.db.gz, add to standalone build

--- make-standalone.bat
+++ make-standalone.bat
@@ -6,6 +6,7 @@ copy TaskbarLib.tlb build\custom-install-standalone
 copy bin\win32\save3ds_fuse.exe build\custom-install-standalone\bin
 copy bin\README build\custom-install-standalone\bin
 copy custom-install-finalize.3dsx build\custom-install-standalone
+copy title.db.gz build\custom-install-standalone
 copy extras\windows-quickstart.txt build\custom-install-standalone
 copy LICENSE.md build\custom-install-standalone
 python -m zipfile -c dist\custom-install-standalone.zip build\custom-install-standalone
