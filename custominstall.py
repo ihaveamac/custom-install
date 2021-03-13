09ed0093dfec5ce92fbf26e336bdc037077b70f7
Ian Burgwin <ian@ianburgwin.net>
2021-03-12 18:24:10 -0800

    custominstall: post-release version bump

--- custominstall.py
+++ custominstall.py
@@ -45,7 +45,7 @@ if is_windows:
 else:
     from os import statvfs
 
-CI_VERSION = '2.1b3'
+CI_VERSION = '2.1b4.dev1'
 
 # used to run the save3ds_fuse binary next to the script
 frozen = getattr(sys, 'frozen', False)
